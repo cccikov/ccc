0f7d1d7a88e26e27303f4e8b8de6f09a29c5d95d
cccikov <522978139@qq.com>
2017-02-10 17:37:48 +0800

    阴阳师记录-第二版

--- yys/js/yys.js
+++ yys/js/yys.js
@@ -1,3 +1,74 @@
+$(function() {
+    var data = {};
+
+    if (theSameDay()) { //同一天
+        console.log("是同一天");
+        data = ccc.getStorage("yys") != undefined ? ccc.getStorage("yys") : ccc.strToJson($("#result").val());
+        $("#result").val(syntaxHighlight(data));
+    } else {
+        console.log("不是同一天");
+        var json = ccc.strToJson($("#result").val());
+        ccc.setStorage("yys", json);
+        ccc.setStorage("time", (new Date()).getTime());
+    }
+
+    // 载入数据
+    for (var i in data) {
+        var id = i,
+            len = data[i].number.length,
+            number = data[i].number; //御魂具体数据
+        for (var j = 0; j < len; j++) {
+            var sum = number[j].sum;
+            var tosum = number[j].tosum;
+            var topInput = $("#" + id).find("li").eq(j).find(">input");
+            topInput.val(sum);
+            var len2 = number[j].tosum.length;
+            for (var k = 0; k < len2; k++) {
+                var input = $("#" + id).find("li").eq(j).find("label input").eq(k);
+                input.val(number[j].tosum[k]);
+            }
+        }
+    }
+
+    /*input输入的时候*/
+    $("input").on("input", function() {
+        var that = $(this);
+        var val = that.val();
+        val = Number(val);
+        that.val(val);
+        var wrap = that.parents(".mitama"); //整个御魂对象
+        var li = that.parents("li");
+        var position = li.index();
+        var levelPosition = that.index() - 1;
+        var id = wrap[0].id; //御魂的拼音，作为id
+        var name = wrap.find("h5").html();
+        if (!data[id]) {
+            data[id] = new mitama(name, id);
+        }
+
+        var numObj = data[id].number;
+        var type = that.parents("label").attr("data-name");
+        var typePosition = that.parents(".type").find("input").index(that); //.type里面的第几个input
+        var input = li.find(">input");
+        numObj[position].tosum[typePosition] = val;
+        var sum = arrSum(numObj[position].tosum);
+        numObj[position].sum = sum;
+        input.val(sum);
+        numObj[position][type][levelPosition] = val;
+
+        ccc.setStorage("yys", data);
+        $('#result').val(syntaxHighlight(data));
+
+    }).on("focus", function() {
+        this.select();
+    });
+
+
+});
+
+
+
+
 /*json格式化*/
 // function syntaxHighlight(json) {
 //     if (typeof json != 'string') {
@@ -47,15 +118,16 @@ function theSameDay() {
     return oldDay == newDay;
 }
 
-/*御魂类型构造函数*/
-
-
 /*御魂构造函数*/
-function mitama(name,id) {
+function mitama(name, id) {
     this.name = name;
     this.id = id;
     this.number = [];
-    this.number[0] = [0, 0, 0];//1号位,只是记不同星的御魂数
+    this.number[0] = {
+        "sum": 0,
+        "tosum": [0, 0, 0],
+        "attack": [0, 0, 0],
+    };
     this.number[1] = {
         "sum": 0,
         "tosum": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -64,7 +136,11 @@ function mitama(name,id) {
         "defense": [0, 0, 0],
         "speed": [0, 0, 0]
     };
-    this.number[2] = [0, 0, 0];
+    this.number[2] = {
+        "sum": 0,
+        "tosum": [0, 0, 0],
+        "defense": [0, 0, 0],
+    };
     this.number[3] = {
         "sum": 0,
         "tosum": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -74,7 +150,11 @@ function mitama(name,id) {
         "effect": [0, 0, 0],
         "resisted": [0, 0, 0]
     };
-    this.number[4] = [0, 0, 0];
+    this.number[4] = {
+        "sum": 0,
+        "tosum": [0, 0, 0],
+        "health": [0, 0, 0],
+    };
     this.number[5] = {
         "sum": 0,
         "tosum": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -85,79 +165,3 @@ function mitama(name,id) {
         "critical-damage": [0, 0, 0]
     };
 }
-
-$(function() {
-    var data = {};
-
-    if (theSameDay()) { //同一天
-        console.log("是同一天");
-        data = ccc.getStorage("yys");
-        console.log(data);
-    } else {
-        console.log("不是同一天");
-        ccc.setStorage("time", (new Date()).getTime());
-    }
-
-    // 载入数据
-    for(var i in data){
-        var id = i,
-        len=data[i].number.length,
-        number = data[i].number;//御魂具体数据
-        for(var j=0;j<len;j++){
-            if (j == 1 || j == 3 || j == 5) { //是2,4,6位置的时候;数组从0开始,0即1
-                var sum = number[j].sum;
-                var tosum = number[j].tosum;
-                var topInput = $("#"+id).find("li").eq(j).find(">input");
-                topInput.val(sum);
-                var len2 = number[j].tosum.length;
-                for(var k=0;k<len2;k++){
-                    var input = $("#"+id).find("li").eq(j).find("label input").eq(k);
-                    input.val(number[j].tosum[k]);
-                }
-            }else{//是1,3,5位置的时候;为数组
-                var len2 = number[j].length;
-                for(var k=0;k<len2;k++){
-                    var input = $("#"+id).find("li").eq(j).find("input").eq(k);
-                    input.val(number[j][k]);
-                }
-            }
-        }
-    }
-
-
-    /*input输入的时候*/
-    $("input").on("input", function() {
-        var that = $(this);
-        var val = that.val();
-        val = Number(val);
-        that.val(val);
-        var wrap = that.parents(".mitama"); //整个御魂对象
-        var li = that.parents("li");
-        var position = li.index();
-        var levelPosition = that.index() - 1;
-        var id = wrap[0].id; //御魂的拼音，作为id
-        var name = wrap.find("h5").html();
-        if (!data[id]) {
-            data[id] = new mitama(name,id);
-        }
-        var numObj = data[id].number;
-        if (position == 1 || position == 3 || position == 5) { //是2,4,6位置的时候
-            var type = that.parents("label").attr("data-name");
-            var typePosition = that.parents(".type").find("input").index(that); //.type里面的第几个input
-            var input = li.find(">input");
-            numObj[position].tosum[typePosition] = val;
-            var sum = arrSum(numObj[position].tosum);
-            numObj[position].sum = sum;
-            input.val(sum);
-            numObj[position][type][levelPosition] = val;
-        } else { //是1,3,5位置的时候
-            numObj[position][levelPosition] = val;
-        }
-        ccc.setStorage("yys", data);
-        $('#result').html(syntaxHighlight(data));
-    }).on("focus", function() {
-        this.select();
-    });
-
-
-});
